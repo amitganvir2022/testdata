3f0820bcab73a49056e9927eeadf8dfe2c35f7a1
root <root@ip-172-31-93-158.ec2.internal>
2023-06-24 12:00:50 +0000

    updating docker image

---
+++ docker-java-image/create-java-docker-image.sh
@@ -0,0 +1,20 @@
+#### TO create docker images on local ####
+cd CREATE-JAVA-APP-DOCKER-IMAGE
+dos2unix *
+docker build . -t amitrepo/hello2java:0.0.1
+
+
+#### Follow belos steps to create docker image on minikube machine ####
+cd CREATE-JAVA-APP-DOCKER-IMAGE
+dos2unix *
+
+## for root user
+repo=$(pwd)
+minikube ssh "cd $repo; docker build . -t amitrepo/hello2java:0.0.1"
+
+## for non-root user
+repo=$(pwd)
+repo=$(echo $repo|sed 's/home/hosthome/')
+minikube ssh "cd $repo; docker build . -t amitrepo/hello2java:0.0.1"
+
+
---
+++ docker-java-image/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -x
+java -jar /myapp/app.jar
