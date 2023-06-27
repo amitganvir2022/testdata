8ec29e97f36cf91bea21605714a3a322061dc059
amitganvir2022 <137477715+amitganvir2022@users.noreply.github.com>
2023-06-27 11:38:59 +0530

    fixed network issue
    app was not able to access from across all nodes

--- kubernetes/k8s-1-21-setup.sh
+++ kubernetes/k8s-1-21-setup.sh
@@ -17,8 +17,10 @@ Create Security Group and all all TCP port as of now on temprory bases. Use this
 #
 
 Step3:
-## Putty(ssh) Master and Worker ec2 and perofrm below commands on both
+## Putty(ssh) Master and Worker ec2 and perofrm below commands on both - Login with root user
 sudo su -
+
+apt-get update -y
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common  -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -34,16 +36,35 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sysctl net.bridge.bridge-nf-call-iptables=1
 
 
-Step4:
-# On master Side
+Step4: # On master Side
 sudo su -
-#kubeadm init --pod-network-cidr=10.244.0.0/16 >> cluster_initialized.txt
 kubeadm init --pod-network-cidr=192.168.0.0/16 >> cluster_initialized.txt
 mkdir /root/.kube
 cp /etc/kubernetes/admin.conf /root/.kube/config
 #kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-kubectl apply -f https://docs.projectcalico.org/archive/v3.20/manifests/calico.yaml
+#kubectl apply -f https://docs.projectcalico.org/archive/v3.20/manifests/calico.yaml
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml -O
+kubectl apply -f calico.yaml
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
 systemctl restart kubelet.service
 kubeadm token create --print-join-command
 
