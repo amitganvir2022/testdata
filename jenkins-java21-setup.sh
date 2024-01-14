57f8027ebbb2243993752c68656afc6ebf21a758
Open Source book <amitganvir6@gmail.com>
2024-01-14 19:39:47 +0530

    Update jenkins-java21-setup.sh

--- jenkins-java21-setup.sh
+++ jenkins-java21-setup.sh
@@ -94,15 +94,19 @@ sudo su -
 apt install maven -y
 
 ## use below command on jenkins jobs UI
-user your own repod  https://github.com/amitganvir2022/hello_maven.git
+user your own repod  https://github.com/amitganvir23/hello_maven.git
 cd lcd
+ls
 mvn clean
 mvn compile
 mvn package
 
-cp lcd/target/lcd-0.0.1-SNAPSHOT.jar lcd.jar
-sudo docker build . -t "amitganvir6/lcd:v${BUILD_NUMBER}"
-sudo docker push amitganvir6/lcd:v${BUILD_NUMBER}
+DOCKER_USERNAME=amitganvir6
+DOCKER_PASSWORD="dckr_pat_FtkB8x_xWiGc9KQ6EqjRgwGbe3o"
+sudo docker login -u $DOCKER_USERNAME -p${DOCKER_PASSWORD}
+cp target/lcd-*-SNAPSHOT.jar lcd.jar
+sudo docker build . -t "$DOCKER_USERNAME/lcd:v${BUILD_NUMBER}"
+sudo docker push $DOCKER_USERNAME/lcd:v${BUILD_NUMBER}
 
 kubectl run lcd --image=amitganvir6/lcd:v${BUILD_NUMBER}
 kubectl expose pod lcd --type=NodePort --port=8082
