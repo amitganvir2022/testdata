bd1b5b768eea17eeeb92736a76bece9aab2bd52b
Open Source book <amitganvir6@gmail.com>
2024-06-06 15:33:49 +0530

    Update k8s-dashboard-setup.sh

--- Nashik-batch-2024/k8s-dashboard-setup.sh
+++ Nashik-batch-2024/k8s-dashboard-setup.sh
@@ -1,5 +1,3 @@
-### NOT WORKING
-
 #### ------ Master side only ------------
 ## Deploy k8s dashboard on Cluster
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
