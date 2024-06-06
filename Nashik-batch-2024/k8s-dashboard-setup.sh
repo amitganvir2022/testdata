b33642feb3636e3d80a98ad0cc639ed12289f233
Open Source book <amitganvir6@gmail.com>
2024-06-06 15:33:37 +0530

    Update k8s-dashboard-setup.sh

--- Nashik-batch-2024/k8s-dashboard-setup.sh
+++ Nashik-batch-2024/k8s-dashboard-setup.sh
@@ -4,35 +4,40 @@
 ## Deploy k8s dashboard on Cluster
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
 
-## Replace ClusterIP to NodePort for servcie account
-kubectl -n kubernetes-dashboard patch svc kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
-
-##Check your External PortNumber <NodePort>.
-kubectl -n kubernetes-dashboard get svc
-
-## Giving Cluster access for service account.
-kubectl create clusterrolebinding deployment-controller --clusterrole=cluster-admin --serviceaccount=kube-system:deployment-controller
+## Crating Service account and giving Full Cluster access.
 cat <<EOF | sudo tee dashboard.rb
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+
+---
+apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
-  name: deployment-controller
+  name: admin-user
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
   name: cluster-admin
 subjects:
 - kind: ServiceAccount
-  name: deployment-controller
-  namespace: kube-system
+  name: admin-user
+  namespace: kubernetes-dashboard
 EOF
 
 kubectl apply -f dashboard.rb
 
-## Get Default Admin-token
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | awk '/^deployment-controller-token-/{print $1}') | awk '$1=="token:"{print $2}'
+## Replace ClusterIP to NodePort for servcie account
+kubectl -n kubernetes-dashboard patch svc kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
 
-## Verify Dashobard On firefox/chrome and Login with Above Admin-token
-https://<ec2-ip>:<NodePort>
+##Check your External PortNumber <NodePort>. Using Below URL
+kubectl -n kubernetes-dashboard get svc
 
- kubectl create token admin-user -n kubernetes-dashboard
+## Create a Admin-token the admin-user account and Login with Token for the below URL
+kubectl create token admin-user -n kubernetes-dashboard
+
+## Verify Dashobard On firefox/chrome with HTTPS and Login with Above Admin-token
+https://<ec2--Public-ip>:<NodePort>
+
